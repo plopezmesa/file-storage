bd0c9797c95ad102db2c5fbd3b8a4759b106f372
plopezmesa <plopezmesa@gmail.com>
2016-07-15 11:55:43 +0200

    FileStorageMetadata constructor using setters

--- src/main/java/es/arcasi/oss/filestorage/model/FileStorageItem.java
+++ src/main/java/es/arcasi/oss/filestorage/model/FileStorageItem.java
@@ -6,27 +6,30 @@ package es.arcasi.oss.filestorage.model;
  *
  */
 public class FileStorageItem {
-  private byte [] file;
+  private byte[] file;
   private FileMetadata fileMetadata;
-  
+
   public FileStorageItem(byte[] file) {
     this.file = file;
   }
-  
+
   public FileStorageItem(byte[] file, FileMetadata fileMetadata) {
-    this.file = file;
-    this.fileMetadata = fileMetadata;
+    setFile(file);
+    setFileMetadata(fileMetadata);
   }
 
   public byte[] getFile() {
     return file;
   }
+
   public void setFile(byte[] file) {
     this.file = file;
   }
+
   public FileMetadata getFileMetadata() {
     return fileMetadata;
   }
+
   public void setFileMetadata(FileMetadata fileMetadata) {
     this.fileMetadata = fileMetadata;
   }
